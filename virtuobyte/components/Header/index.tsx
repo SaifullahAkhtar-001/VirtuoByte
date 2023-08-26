73745191e15bb371c2851345fea4037bc1e7a1e5
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-08-26 05:29:12 -0400

    added theme toggler

---
+++ virtuobyte/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ThemeToggler from "./ThemeToggle";
+
+const index: React.FC = () => {
+  return (
+    <div>
+      <ThemeToggler />
+    </div>
+  );
+};
+
+export default index;
