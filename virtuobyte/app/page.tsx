c0b75de41edb3a85c3aed2ee0055170a92ed1688
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-08-26 13:27:43 -0400

    update

--- virtuobyte/app/page.tsx
+++ virtuobyte/app/page.tsx
@@ -1,11 +1,9 @@
-import React from "react";
+import React from 'react'
 
-const page: React.FC = () => {
+const page:React.FC = () => {
   return (
-    <div className="dark:text-white text-black duration-300 transition ease-in-out ">
-      page
-    </div>
-  );
-};
+    <></>
+  )
+}
 
-export default page;
+export default page
--- virtuobyte/components/Header/index.tsx
+++ virtuobyte/components/Header/index.tsx
@@ -1,12 +1,68 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import ThemeToggler from "./ThemeToggle";
-
-const index: React.FC = () => {
+import menuData from "./menuData";
+import Link from "next/link";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { useTheme } from "next-themes";
+import { Menu } from "@/types/menu";
+const Navbar: React.FC = () => {
+  const [menu, setMenu] = useState(false);
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+  const { theme, setTheme } = useTheme();
+  
   return (
-    <div>
-      <ThemeToggler />
-    </div>
+    <>
+      <div className="flex items-center justify-between transition duration-300 ease-in-out">
+        <div className="z-[50]">Logo</div>
+
+        <div className="flex flex-col items-center gap-4">
+          <div className="flex items-center gap-4 z-[50]">
+            <RxHamburgerMenu
+              size={28}
+              className="dark:text-white cursor-pointer text-black "
+              onClick={() => {
+                setMenu(!menu);
+              }}
+            />
+            <ThemeToggler />
+          </div>
+        </div>
+      </div>
+      {menu && (
+        <div id="menu">
+          <div id="menu-items">
+            {menuData.map((data: Menu) => (
+              <Link
+                href={data.path!}
+                key={data.id}
+                className={`text-black dark:text-white menu-item ${
+                  activeIndex === data.id ? "active" : ""
+                }`}
+                onMouseOver={() => {
+                  setActiveIndex(data.id);
+                }}
+              >
+                {data.title}
+              </Link>
+            ))}
+          </div>
+          <div
+            id={
+              theme === "dark"
+                ? "menu-background-pattern-dark"
+                : "menu-background-pattern"
+            }
+            style={{
+              backgroundPosition: `0% -${
+                activeIndex ? (activeIndex + 1) * 25 : 0
+              }%`,
+            }}
+          ></div>
+        </div>
+      )}
+    </>
   );
 };
 
-export default index;
+export default Navbar;
