f4f2389ad6c43782aecc0fdc80cc9d118a4e97d9
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-09-01 14:41:46 -0400

    update

--- virtuobyte/components/Hero/index.tsx
+++ virtuobyte/components/Hero/index.tsx
@@ -12,13 +12,13 @@ const Index: React.FC = () => {
     offset: ["end end", "end start"]
 
   });
-  const opacity = useTransform(scrollYProgress, [0.4, 1], [1, 0])
+  const opacity = useTransform(scrollYProgress, [0, 0.9], [1, 0])
 
   return (
     <motion.div style={{ opacity }} id='top' ref={targetRef} className='flex min-[1201px]:items-center max-[1200px]:mt-[4rem]  gap-4 justify-between min-h-screen max-[1200px]:flex-col '>
       <div className='flex flex-col gap-[4rem] max-[450px]:gap-[2rem]  flex-[0.8] justify-between '>
         <div className='flex flex-col  gap-4'>
-          <div className='text-[35px] max-[500px]:text-[28px] max-[400px]:text-[22px] font-[500] break-words '>
+          <div className='text-[35px] max-[450px]:text-[28px] max-[386px]:text-[25px]  font-[500] break-words '>
             Elevating Businesses through Innovative Software Solutions and Digital Excellence.
           </div>
           <div className='text-[14px] break-words'>Discover how our innovative software solutions and commitment to digital excellence can revolutionize your business operations. We&apos;re dedicated to elevating businesses by seamlessly integrating cutting-edge technology, optimizing processes, and delivering exceptional customer experiences. Explore the limitless possibilities with our tailored solutions, and let us be your partner in achieving sustainable growth and success.</div>
--- virtuobyte/components/ScrollTo/index.tsx
+++ virtuobyte/components/ScrollTo/index.tsx
@@ -5,9 +5,9 @@ import React from 'react'
 import { BsArrowUp } from 'react-icons/bs';
 const index: React.FC<{ top: boolean }> = (top) => {
     return (
-        
-            <Link href={'#top'} className='flex flex-col fixed bottom-3 right-3 gap-2 border w-[60px] text-center border-black justify-center p-2 rounded-lg break-words  items-center text-black font-[500]'><BsArrowUp size={22}/>Scroll Up</Link>
-        
+
+        <Link href={'#top'} className='flex flex-col fixed bottom-5 right-5 gap-2 border  text-center dark:border-white dark:text-white border-black justify-center p-2 rounded-lg break-words  items-center  font-[500]'><BsArrowUp size={22} className='dark:text-white' /></Link>
+
     )
 }
 
