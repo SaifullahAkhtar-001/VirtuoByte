7c28d8e771b54f261549fa8ce9e674dc0c8df3e0
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-09-01 13:17:35 -0400

    update

--- virtuobyte/components/Header/index.tsx
+++ virtuobyte/components/Header/index.tsx
@@ -39,7 +39,7 @@ const Navbar: React.FC = () => {
               />
             )}
 
-            {/* <ThemeToggler /> */}
+            <ThemeToggler />
           </div>
         </div>
       </div>
--- virtuobyte/components/Hero/index.tsx
+++ virtuobyte/components/Hero/index.tsx
@@ -22,7 +22,7 @@ const Index: React.FC = () => {
           <div className='text-[35px] max-[500px]:text-[28px] max-[400px]:text-[22px] font-[500] break-words '>
           Elevating Businesses through Innovative Software Solutions and Digital Excellence.
         </div>
-          <div className='text-[14px] break-words'>Discover how our innovative software solutions and commitment to digital excellence can revolutionize your business operations. We're dedicated to elevating businesses by seamlessly integrating cutting-edge technology, optimizing processes, and delivering exceptional customer experiences. Explore the limitless possibilities with our tailored solutions, and let us be your partner in achieving sustainable growth and success.</div>
+          <div className='text-[14px] break-words'>Discover how our innovative software solutions and commitment to digital excellence can revolutionize your business operations. We&apos;re dedicated to elevating businesses by seamlessly integrating cutting-edge technology, optimizing processes, and delivering exceptional customer experiences. Explore the limitless possibilities with our tailored solutions, and let us be your partner in achieving sustainable growth and success.</div>
         </div>
 
         <div className='flex max-[1200px]:flex-col min-[12001px]:items-center gap-8'>
