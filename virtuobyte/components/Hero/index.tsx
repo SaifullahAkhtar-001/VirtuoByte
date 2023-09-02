2a3157ee63ac888adf9a4a095ea5d4811fb59b44
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-09-02 13:08:37 -0400

    update

--- virtuobyte/components/Hero/index.tsx
+++ virtuobyte/components/Hero/index.tsx
@@ -15,10 +15,10 @@ const Index: React.FC = () => {
   const opacity = useTransform(scrollYProgress, [0, 0.9], [1, 0])
 
   return (
-    <div  id='top' ref={targetRef} className='flex min-[1201px]:items-center max-[1200px]:mt-[4rem]  gap-4 justify-between min-h-screen max-[1200px]:flex-col '>
-      <div className='flex flex-col gap-[4rem] max-[450px]:gap-[2rem]  flex-[0.8] justify-between '>
+    <motion.div style={{opacity}}  id='top' ref={targetRef} className='flex min-[1201px]:items-center max-[1200px]:mt-[4rem]  gap-4 justify-between min-h-screen max-[1200px]:flex-col '>
+      <div className='flex flex-col gap-[4rem] max-[450px]:gap-[2rem]  flex-1 justify-between '>
         <div className='flex flex-col  gap-4'>
-          <div className='text-[45px] max-[450px]:text-[28px] max-[386px]:text-[25px]  font-[500] break-words '>
+          <div className='text-[45px] max-[550px]:text-[28px] max-[386px]:text-[25px]  font-[600] break-words '>
             Elevating Businesses through Innovative Software Solutions and Digital Excellence.
           </div>
           <div className='text-[15px] break-words'>Discover how our innovative software solutions and commitment to digital excellence can revolutionize your business operations. We&apos;re dedicated to elevating businesses by seamlessly integrating cutting-edge technology, optimizing processes, and delivering exceptional customer experiences. Explore the limitless possibilities with our tailored solutions, and let us be your partner in achieving sustainable growth and success.</div>
@@ -29,8 +29,8 @@ const Index: React.FC = () => {
           <button className='underline underline-offset-4 flex items-center gap-1 '>Contact Us <FiArrowUpRight size={17} /></button>
         </div>
       </div>
-      <Image src={HeroSVG} height={100} width={100} alt='Hero Image' className='w-full flex flex-1  ' />
-    </div>
+      <Image src={HeroSVG} height={0} width={0} alt='Hero Image' className='flex flex-1' />
+    </motion.div>
   );
 }
 
--- virtuobyte/components/Skills/index.tsx
+++ virtuobyte/components/Skills/index.tsx
@@ -31,7 +31,7 @@ const Skills: React.FC = () => {
   }
   return (
     <div ref={refContainer} className=' dark:text-white text-black'>
-      <div className='min-h-screen  flex flex-col justify-center items-center text-[3rem] max-[782px]:text-[1.5rem] max-[500px]:text-[28px] my-[6rem] min-[10000px]:m-8 tracking-wide font-[500]'>
+      <div className='min-h-screen  flex flex-col justify-center items-center text-[3rem] max-[782px]:text-[1.5rem] max-[500px]:text-[28px] my-[6rem] min-[10000px]:m-8 tracking-wide font-[600]'>
         <div className='leading-[1.15] flex flex-col  gap-14 min-[500px]:items-center break-words min-[500px]:text-center'>
           <div
             className={s.skillText}
