6723126898df02b862a01f4813bf71de18be7da4
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-09-06 13:05:14 -0400

    Update index.tsx

--- virtuobyte/components/About/index.tsx
+++ virtuobyte/components/About/index.tsx
@@ -1,25 +1,25 @@
-import React from 'react'
+import React from 'react';
 
 const index = () => {
     return (
         <>
-            <div id='about' className='flex gap-4 max-[1000px]:flex-col justify-between my-4  '>
-                <div className='flex flex-1 flex-col gap-5 justify-between  border dark:border-white border-black rounded-xl max-[425px]:p-4  p-6'>
+            <div id='about' className='flex gap-4 max-[1000px]:flex-col justify-between my-4'>
+                <div className='flex flex-1 flex-col gap-5 justify-between border dark:border-white border-black rounded-xl max-[425px]:p-4 p-6'>
                     <div className='text-[28px] max-[425px]:text-[25px] font-[500] break-words'>What We Do?</div>
                     <div className='break-words text-[16px]'>
-                        At Virtuo Byte, we specialize in crafting tailored web services and solutions to enhance businesses' online presence. Our dedicated team of experts leverages cutting-edge technology and industry best practices to ensure your digital success. Whether you're a startup looking to establish a strong online presence or an established corporation aiming to streamline operations, we have the knowledge and experience to create effective, future-proof solutions.
+                        At Virtuo Byte, we specialize in crafting tailored web services and solutions to enhance businesses&apos; online presence. Our dedicated team of experts leverages cutting-edge technology and industry best practices to ensure your digital success. Whether you&apos;re a startup looking to establish a strong online presence or an established corporation aiming to streamline operations, we have the knowledge and experience to create effective, future-proof solutions.
                     </div>
                 </div>
               
-                <div className='flex flex-1 flex-col gap-5 border dark:border-white border-black  rounded-xl max-[425px]:p-4 p-6'>
+                <div className='flex flex-1 flex-col gap-5 border dark:border-white border-black rounded-xl max-[425px]:p-4 p-6'>
                     <div className='text-[28px] max-[425px]:text-[25px] font-[500] break-words'>Why Choose Us?</div>
                     <div className='break-words text-[16px]'>
-                        <ul className=" flex flex-col gap-3 pl-2 ">
+                        <ul className="flex flex-col gap-3 pl-2">
                             <li><strong>Client-Centric Approach:</strong> We prioritize your unique needs and goals.</li>
                             <li><strong>Cutting-Edge Technology:</strong> We employ the latest tools to give you a competitive edge.</li>
                             <li><strong>Proven Track Record:</strong> Our portfolio showcases successful projects.</li>
                             <li><strong>Dedicated Support:</strong> We offer ongoing maintenance and support.</li>
-                            <li><strong>Innovation:</strong> We're your innovation partners, keeping you ahead in the digital race.</li>
+                            <li><strong>Innovation:</strong> We&apos;re your innovation partners, keeping you ahead in the digital race.</li>
                         </ul>
                     </div>
                 </div>
@@ -28,4 +28,4 @@ const index = () => {
     )
 }
 
-export default index
+export default index;
